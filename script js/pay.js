a326514e8168622384b01fcda1d1c0155c2b09ad
MD Ibna Abid <mdabout2@gmail.com>
2026-02-20 10:29:35 +0600

    edit more fetued

--- script js/pay.js
+++ script js/pay.js
@@ -2,41 +2,35 @@ document.getElementById("pay-money").addEventListener("click", function(){
 
     const pay = document.getElementById("pay-selector").value;
 
-    if(pay === "select method"){
-        alert("select a method");
+    if(pay === "select bank"){
+        alert("Select a bank");
         return;
     }
 
-    const payAccount = document.getElementById("bank-accountNum").value;
+    const payAccount = document.getElementById("pay-accountNum").value;
 
-    if(payAccount.length == 11){
-        alert("valid number");
-        
+    if(payAccount.length !== 11){
+        alert("Invalid account number");
+        return;
     }
 
-    const payAmount= document.getElementById("pay-amount").value;
-    const reserveAmount= document.getElementById("amountauto");
-    const currentBalance= Number(reserveAmount.innerText);
-
-    const total= currentBalance + Number(payAmount);
-    
-     reserveAmount.innerText = total
-     alert("pay money successfully")
-    
-     
-    const passwordOfId= document.getElementById("pay-pin");
-    const pass = passwordOfId.value;
-    
-    if(pass ==="1234"){
-        alert("add money successfully");
-    
-
-    }
-    else{
-        alert("failed");
-        return
+    const payAmount = Number(document.getElementById("pay-amount").value);
+    if(payAmount <= 0){
+        alert("Enter valid amount");
+        return;
     }
 
+    const pass = document.getElementById("pay-pin").value;
+    if(pass !== "1234"){
+        alert("Invalid pin");
+        return;
+    }
 
+    const reserveAmount = document.getElementById("amountauto");
+    const currentBalance = Number(reserveAmount.innerText);
+
+    reserveAmount.innerText = currentBalance - payAmount;
+
+    alert("Pay money successfully");
 
 });
